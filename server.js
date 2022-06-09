ab6736e039f9f82fc76b5fc31c071469dc93f5f1
Patrick Loser <patrick.los@proton.me>
2022-06-09 10:08:10 +0200

    finished basic api

--- server.js
+++ server.js
@@ -3,11 +3,50 @@ const bodyParser = require('body-parser');
 
 const port = 3000;
 const app = express();
+let students = [{"id":1, "name":"Deniz"},{"id":2, "name":"Dustin"}]
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/students', (req, res) => {
+    res.send(students);
+    logSend(students);
+});
+
+app.get('/student/:id', (req, res) => {
+    id = req.params.id;
+    let student = students.find(element => element.id == id)
+    res.send(student);
+    logSend(student);
+});
+
+app.post('/students', (req, res) => {
+    let student = req.body;
+    students.push(student)
+    res.send(student)
+    console.log(student,"was posted");
+});
+
+app.delete('/student/:id', (req, res) => {
+    id = req.params.id;
+    let student = students.findIndex(element => element.id == id)
+    students.splice(student,1)
+
+    res.send(students);
+});
+
+app.put('/student/:id', (req, res) => {
+    let id = req.params.id;
+    let data = req.body
+    let student = students.findIndex(element => element.id == id)
+    students[student] = data
+    res.send(students);
+    logSend(students);
+});
+
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
 
-app.get('/', (req, res) => {
-    res.send('Hello World, from express');
-});
+function logSend(data){
+    console.log(data, "was sent")
+}
